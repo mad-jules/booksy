c66fd666a0a93de48af0fa47c0c98c440183d176
mad-jules <yuliia.pechera@gmail.com>
2026-02-28 20:13:44 +0200

    feat: add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Route, Routes } from "react-router";
+import RootLayout from "./components/RootLayout/RootLayout";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+
 export default function App() {
-  return <>App</>;
+  return (
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
+    </Routes>
+  );
 }
